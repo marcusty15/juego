e131055db538462cd70980577c8c6f630a86b73c
marcusty15 <marco.troconis15@gmail.com>
2022-05-30 20:11:29 -0400

    se agrego la salida del resultado

--- js.js
+++ js.js
@@ -1,27 +1,29 @@
+const getRandomArbitrary = (participantes, min, max) => {
+  return participantes[Number((Math.random() * (max - min) + min).toFixed(0))];
+};
+const sorteo = ["piedra", "papel", "tijera"];
+// console.log(
+//   "El ganador es:" + getRandomArbitrary(sorteo, 0, sorteo.length - 1)
+//);
 
-    
-    const getRandomArbitrary = (participantes, min, max)=>{
-        return participantes [Number((Math.random() * (max - min) + min).toFixed(0))]
-    }
-        const sorteo = ["piedra", 'papel', 'tijera']
-        console.log("El ganador es:" + getRandomArbitrary(sorteo, 0, (sorteo.length-1)))
-
-    let entrada = document.querySelector('#input').value
-    const juego = (event)=>{
-        event.preventDefault();
-        if(getRandomArbitrary===entrada.value){
-            alert("empate, sigue jugando")
-        } else if(getRandomArbitrary==="tijera" && entrada.value === "piedra"){
-            alert('Ganaste!!!')
-        } else if(getRandomArbitrary==="tijera" && entrada.value === "papel"){
-            alert('Perdiste!!!')
-        } else if(getRandomArbitrary==="piedra" && entrada.value === "papel"){
-            alert('Ganaste!!!')
-        } else if(getRandomArbitrary==="piedra" && entrada.value === "tijera"){
-            alert('Perdiste!!!')
-        } else if(getRandomArbitrary==="papel" && entrada.value === "piedra"){
-            alert('Ganaste!!!')
-        } else if(getRandomArbitrary==="papel" && entrada.value === "tijera"){
-            alert('Perdiste!!!')
-        } 
-    }
+let entrada = document.querySelector("#input");
+let resultado = document.querySelector(".resultado")
+alert(entrada.value);
+const juego = (event) => {
+  event.preventDefault();
+  if (getRandomArbitrary(sorteo, 0, sorteo.length - 1) === entrada.value) {
+    alert("empate, sigue jugando");
+  } else if (getRandomArbitrary(sorteo, 0, sorteo.length - 1) === "tijera" && entrada.value === "piedra") {
+    resultado.innerHTML = `<h2>"Ganaste!!!"</h2>`;
+  } else if (getRandomArbitrary(sorteo, 0, sorteo.length - 1) === "tijera" && entrada.value === "papel") {
+    resultado.innerHTML =`<h2>"Perdiste!!!"</h2>`;
+  } else if (getRandomArbitrary(sorteo, 0, sorteo.length - 1) === "piedra" && entrada.value === "papel") {
+    resultado.innerHTML =`<h2>"Ganaste!!!"</h2>`;
+  } else if (getRandomArbitrary(sorteo, 0, sorteo.length - 1) === "piedra" && entrada.value === "tijera") {
+    resultado.innerHTML =`<h2>"Perdiste!!!"</h2>`;
+  } else if (getRandomArbitrary(sorteo, 0, sorteo.length - 1) === "papel" && entrada.value === "piedra") {
+    resultado.innerHTML =`<h2>"Ganaste!!!"</h2>`;
+  } else if (getRandomArbitrary(sorteo, 0, sorteo.length - 1) === "papel" && entrada.value === "tijera") {
+    resultado.innerHTML =`<h2>"Perdiste!!!"</h2>`;
+  }
+};
