dc966b5b0cd50108df5cec4f8b34c4d4024e60c5
marcusty15 <marco.troconis15@gmail.com>
2022-05-31 12:16:34 -0400

    se quitaron lineas sin uso

--- js.js
+++ js.js
@@ -46,29 +46,4 @@ const juego = (event) => {
   
 };
 
-// let computerSelection = getRandomArbitrary(sorteo, 0, sorteo.length - 1);
-// let entrada = document.querySelector("#input").value;
-// let resultado = document.querySelector(".resultado");
 
-// console.log(
-//   "El ganador es:" + getRandomArbitrary(sorteo, 0, sorteo.length - 1)
-// );
-
-// console.log(result);
-// console.log(getResult);
-// if (computerSelection === entrada.value) {
-//   alert("empate, sigue jugando");
-// } else if (computerSelection === "tijera" && entrada.value === "piedra") {
-//   resultado.innerHTML = `<h2>"Ganaste!!!"</h2>`;
-// } else if (computerSelection === "tijera" && entrada.value === "papel") {
-//   resultado.innerHTML = `<h2>"Perdiste!!!"</h2>`;
-// } else if (computerSelection === "piedra" && entrada.value === "papel") {
-//   resultado.innerHTML = `<h2>"Ganaste!!!"</h2>`;
-// } else if (computerSelection === "piedra" && entrada.value === "tijera") {
-//   resultado.innerHTML = `<h2>"Perdiste!!!"</h2>`;
-// } else if (computerSelection === "papel" && entrada.value === "piedra") {
-//   resultado.innerHTML = `<h2>"Ganaste!!!"</h2>`;
-// } else if (computerSelection === "papel" && entrada.value === "tijera") {
-//   resultado.innerHTML = `<h2>"Perdiste!!!"</h2>`;
-// }
-// entrada.value = "";
